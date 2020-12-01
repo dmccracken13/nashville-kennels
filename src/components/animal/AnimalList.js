3a39da6d3089f3a2b5e92e51611c1b206ce14e9c
Danny McCracken <dannymccracken13@yahoo.com>
2020-12-01 15:46:12 -0600

    Added animal detail component and refactored functionality for module

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,50 +1,30 @@
-import React, { useContext, useEffect } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
-import { LocationContext } from "../location/LocationProvider"
-import { CustomerContext } from "../customer/CustomerProvider"
-import { Animal } from "./Animal"
+import Animal from "./Animal"
 import "./Animal.css"
 
-export const AnimalList = (props) => {
-    // This state changes when `getAnimals()` is invoked below
-    const { animals, getAnimals } = useContext(AnimalContext)
-    const { locations, getLocations } = useContext(LocationContext)
-    const { customers, getCustomers } = useContext(CustomerContext)
-    /*
-        What's the effect this is reponding to? Component was
-        "mounted" to the DOM. React renders blank HTML first,
-        then gets the data, then re-renders.
-    */
-    useEffect(() => {
-        // console.log("AnimalList: Initial render before data")
-        getLocations()
-        .then(getCustomers)
-        .then(getAnimals)
+export const AnimalList = ({ history }) => {
+    const { getAnimals, animals } = useContext(AnimalContext)
+
+    // Initialization effect hook -> Go get animal data
+    useEffect(()=>{
+        getAnimals()
     }, [])
 
-    /*
-        This effect is solely for learning purposes. The effect
-        it is responding to is that the animal state changed.
-    */
-    // useEffect(() => {
-        // console.log("AnimalList: Location state changed")
-        // console.log(animals)
-    // }, [animals])
-
     return (
-        <div className="animals">
-            <button onClick={() => props.history.push("/animals/create")}>
-            Make Appointment
+        <>
+            <h1>Animals</h1>
+
+            <button onClick={() => history.push("/animals/create")}>
+                Make Reservation
             </button>
-            {animals.map(animal => {
-        const owner = customers.find(c => c.id === animal.customerId)
-        const clinic = locations.find(l => l.id === animal.locationId)
-    
-        return <Animal key={animal.id}
-                    location={clinic}
-                    customer={owner}
-                    animal={animal} />
-        })}
-        </div>
+            <div className="animals">
+                {
+                    animals.map(animal => {
+                        return <Animal key={animal.id} animal={animal} />
+                    })
+                }
+            </div>
+        </>
     )
 }
--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -28,6 +28,10 @@ export const AnimalProvider = (props) => {
         })
             .then(getAnimals)
     }
+    const getAnimalById = (id) => {
+        return fetch(`http://localhost:8088/animals/${ id }?_expand=location&_expand=customer`)
+            .then(res => res.json())
+    }
 
     /*
         You return a context provider which has the
@@ -37,7 +41,7 @@ export const AnimalProvider = (props) => {
     */
     return (
         <AnimalContext.Provider value={{
-            animals, addAnimal, getAnimals
+            animals, addAnimal, getAnimals, getAnimalById
         }}>
             {props.children}
         </AnimalContext.Provider>
